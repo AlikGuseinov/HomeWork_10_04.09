ece68768d2fb044b5632fbd2aa9e46eddd548c46
Alik <9774952689@bk.ru>
2022-09-04 00:40:57 +0300

    Completed10Task

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.Write("Введите трёхзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a>999 || a<100)
+{
+Console.WriteLine("Введённое вами число не трёхзначное");
+}
+else
+{
+    int a1 =((a / 10) % 10);
+Console.WriteLine("Вторая цифра введённого числа: " + a1);
+}
